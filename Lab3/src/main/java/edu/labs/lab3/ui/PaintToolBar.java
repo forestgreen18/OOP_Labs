539445adbe8354d8a507af71c7103343181f70a2
Illia Kornyk <95077778+forestgreen18@users.noreply.github.com>
2023-11-20 22:59:02 +0200

    feat: add images to the toolbar buttons

--- Lab3/src/main/java/edu/labs/lab3/ui/PaintToolBar.java
+++ Lab3/src/main/java/edu/labs/lab3/ui/PaintToolBar.java
@@ -10,26 +10,79 @@ public class PaintToolBar {
 
     public ToolBar createToolBar() {
 
-        String svgFilePath = "F:\\Labs\\OOP\\Lab3\\src\\main\\resources\\edu\\labs\\lab3\\images\\ellipse.svg";
-        String svgContent = SVGReader.readSVGFile(svgFilePath);
+        double buttonHeight = 50;  // Replace with your desired button height
 
-        SVGPath svgPath = new SVGPath();
-        svgPath.setContent(svgContent);
 
-        Button button = new Button();
-        button.setGraphic(svgPath);
+        String svgEllipseFilePath = "F:\\Labs\\OOP\\Lab3\\src\\main\\resources\\edu\\labs\\lab3\\images\\ellipse.svg";
+        String svgEllipseContent = SVGReader.readSVGFile(svgEllipseFilePath);
+        SVGPath svgEllipsePath = new SVGPath();
+        svgEllipsePath.setContent(svgEllipseContent);
+
+        Button ellipseButton = new Button();
+        ellipseButton.setGraphic(svgEllipsePath);
+
+        String svgRectangleFilePath = "F:\\Labs\\OOP\\Lab3\\src\\main\\resources\\edu\\labs\\lab3\\images\\rectangle.svg";
+        String svgRectangleContent = SVGReader.readSVGFile(svgRectangleFilePath);
+        SVGPath svgRectanglePath = new SVGPath();
+        svgRectanglePath.setContent(svgRectangleContent);
+
+        Button rectangleButton = new Button();
+        rectangleButton.setGraphic(svgRectanglePath);
+
+        String svgPointFilePath = "F:\\Labs\\OOP\\Lab3\\src\\main\\resources\\edu\\labs\\lab3\\images\\point.svg";
+        String svgPointContent = SVGReader.readSVGFile(svgPointFilePath);
+        SVGPath svgPointPath = new SVGPath();
+        svgPointPath.setContent(svgPointContent);
+
+        Button pointButton = new Button();
+        pointButton.setGraphic(svgPointPath);
+
+        String svgLineFilePath = "F:\\Labs\\OOP\\Lab3\\src\\main\\resources\\edu\\labs\\lab3\\images\\line.svg";
+        String svgLineContent = SVGReader.readSVGFile(svgLineFilePath);
+        SVGPath svgLinePath = new SVGPath();
+        svgLinePath.setContent(svgLineContent);
+
+        Button lineButton = new Button();
+        lineButton.setGraphic(svgLinePath);
+
+        String svgEraseFilePath = "F:\\Labs\\OOP\\Lab3\\src\\main\\resources\\edu\\labs\\lab3\\images\\erase.svg";
+        String svgEraseContent = SVGReader.readSVGFile(svgEraseFilePath);
+        SVGPath svgErasePath = new SVGPath();
+        svgErasePath.setContent(svgEraseContent);
+
+        Button eraseButton = new Button();
+        eraseButton.setGraphic(svgErasePath);
+
+
+        ellipseButton.setMinHeight(buttonHeight);
+        ellipseButton.setMaxHeight(buttonHeight);
+        ellipseButton.setPrefHeight(buttonHeight);
+
+        rectangleButton.setMinHeight(buttonHeight);
+        rectangleButton.setMaxHeight(buttonHeight);
+        rectangleButton.setPrefHeight(buttonHeight);
+
+        pointButton.setMinHeight(buttonHeight);
+        pointButton.setMaxHeight(buttonHeight);
+        pointButton.setPrefHeight(buttonHeight);
+
+        lineButton.setMinHeight(buttonHeight);
+        lineButton.setMaxHeight(buttonHeight);
+        lineButton.setPrefHeight(buttonHeight);
+
+        eraseButton.setMinHeight(buttonHeight);
+        eraseButton.setMaxHeight(buttonHeight);
+        eraseButton.setPrefHeight(buttonHeight);
+
+
 
         ToolBar toolBar = new ToolBar(
-                button,
-                new Button("Open"),
-                new Button("Save"),
+                ellipseButton,
+                rectangleButton,
+                pointButton,
+                lineButton,
                 new Separator(),
-                new Button("Clean"),
-                new Button("Compile"),
-                new Button("Run"),
-                new Separator(),
-                new Button("Debug"),
-                new Button("Profile")
+                eraseButton
         );
         return toolBar;
     }
