06f39e66cf64eeef21405c04543b19068426f596
Illia Kornyk <95077778+forestgreen18@users.noreply.github.com>
2023-12-09 12:34:33 +0200

    refactor: rid of redundant getters and setters in LineShape

--- Lab4/src/main/java/edu/labs/lab4/shapes/LineShape.java
+++ Lab4/src/main/java/edu/labs/lab4/shapes/LineShape.java
@@ -4,58 +4,19 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class LineShape extends Shape {
-    private double startX;
-    private double startY;
-    private double endX;
-    private double endY;
+
     private GraphicsContext gc;
 
     public LineShape(double startX, double startY, double endX, double endY, GraphicsContext gc) {
         super(startX, startY, endX, endY);
-        this.startX = startX;
-        this.startY = startY;
-        this.endX = endX;
-        this.endY = endY;
         this.gc = gc;
-    }
-
-    public double getStartX() {
-        return startX;
-    }
-
-    public void setStartX(double startX) {
-        this.startX = startX;
-    }
-
-    public double getStartY() {
-        return startY;
-    }
-
-    public void setStartY(double startY) {
-        this.startY = startY;
-    }
-
-    public double getEndX() {
-        return endX;
-    }
-
-    public void setEndX(double endX) {
-        this.endX = endX;
-    }
-
-    public double getEndY() {
-        return endY;
-    }
-
-    public void setEndY(double endY) {
-        this.endY = endY;
     }
 
     @Override
     public void draw(GraphicsContext gc) {
         gc.setStroke(Color.BLACK);
         gc.setLineWidth(1);
-        gc.strokeLine(startX, startY, endX, endY);
+        gc.strokeLine(this.getStartX(), this.getStartY(), this.getEndX(), this.getEndY());
     }
 
     @Override
@@ -63,7 +24,7 @@ public class LineShape extends Shape {
         gc.setStroke(strokeColor);
         gc.setLineWidth(1);
         gc.setLineDashes(10);
-        gc.strokeLine(startX, startY, endX, endY);
+        gc.strokeLine(this.getStartX(), this.getStartY(), this.getEndX(), this.getEndY());
         gc.setLineDashes(0);
     }
 
@@ -78,6 +39,6 @@ public class LineShape extends Shape {
 
     @Override
     public LineShape clone() {
-        return new LineShape(this.startX, this.startY, this.endX, this.endY, this.gc);
+        return new LineShape(this.getStartX(), this.getStartY(), this.getEndX(), this.getEndY(), this.gc);
     }
 }
