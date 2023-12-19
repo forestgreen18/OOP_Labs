4a07164df89bceaa00ecbe71aacbf9b5dc172dd3
Illia Kornyk <95077778+forestgreen18@users.noreply.github.com>
2023-12-19 10:21:08 +0200

    refactor: cleanup code for lab4

--- Lab4/src/main/java/edu/labs/lab4/Editor.java
+++ Lab4/src/main/java/edu/labs/lab4/Editor.java
@@ -2,106 +2,107 @@ package edu.labs.lab4;
 
 
 import edu.labs.lab4.shapes.Shape;
+import java.io.IOException;
+import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
 public class Editor extends Application {
-    private ArrayList<Shape> shapes = new ArrayList<>(108);
 
-    private Shape currentShape;
+  private final ArrayList<Shape> shapes = new ArrayList<>(108);
 
-    private int shapeCount = 0;
+  private Shape currentShape;
 
-    private boolean isDrawing;
+  private int shapeCount = 0;
 
-    private GraphicsContext gc;
+  private boolean isDrawing;
+
+  private final GraphicsContext gc;
 
 
-    public Editor(GraphicsContext gc) {
-        this.gc = gc;
+  public Editor(GraphicsContext gc) {
+    this.gc = gc;
+  }
+
+  public static void main(String[] args) {
+    launch();
+  }
+
+  @Override
+  public void start(Stage stage) throws IOException {
+
+  }
+
+  public void addShape(Shape shape) {
+    if (shapes.size() < 108) {
+      shapes.add(shape);
+      redrawShapes();
     }
+  }
 
-    @Override
-    public void start(Stage stage) throws IOException {
+  public void redrawShapes() {
+    if (gc != null) {
+      // Clear the canvas
+      gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
 
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
-
-    public void addShape(Shape shape) {
-        if (shapes.size() < 108) {
-            shapes.add(shape);
-            redrawShapes();
+      // Redraw all shapes
+      for (Shape shape : shapes) {
+        if (shape != null) {
+          shape.draw(gc);
         }
+      }
+
+      // Draw the new shape if isDrawing is true
+      if (isDrawing) {
+        currentShape.drawPreviewShape(currentShape.getStartX(), currentShape.getStartY(),
+            currentShape.getEndX(), currentShape.getEndY());
+      }
+    } else {
+      System.out.println("gc is null. The start() method might not have been called yet.");
+    }
+  }
+
+  public void clearCanvas() {
+    // Clear the canvas
+    if (gc != null) {
+      gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
     }
 
-    public void redrawShapes() {
-        if (gc != null) {
-            // Clear the canvas
-            gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+    // Reset the shapes array
+    shapes.clear();
+    shapeCount = 0;
+  }
 
-            // Redraw all shapes
-            for (Shape shape : shapes) {
-                if (shape != null) {
-                    shape.draw(gc);
-                }
-            }
 
-            // Draw the new shape if isDrawing is true
-            if (isDrawing) {
-                currentShape.drawPreviewShape(currentShape.getStartX(), currentShape.getStartY(), currentShape.getEndX(), currentShape.getEndY());
-            }
-        } else {
-            System.out.println("gc is null. The start() method might not have been called yet.");
-        }
+  public void draw(MouseEvent event) {
+
+    double x = event.getX();
+    double y = event.getY();
+    switch (event.getEventType().getName()) {
+      case "MOUSE_PRESSED":
+        currentShape = currentShape.clone();
+        isDrawing = true;
+        currentShape.setStartX(x);
+        currentShape.setStartY(y);
+        currentShape.setEndX(x);
+        currentShape.setEndY(y);
+        break;
+      case "MOUSE_DRAGGED":
+
+        currentShape.setEndX(x);
+        currentShape.setEndY(y);
+        this.redrawShapes();
+        break;
+      case "MOUSE_RELEASED":
+        isDrawing = false;
+        this.addShape(currentShape);
+        break;
     }
+  }
 
-    public void clearCanvas() {
-        // Clear the canvas
-        if (gc != null) {
-            gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
-        }
-
-        // Reset the shapes array
-        shapes.clear();
-        shapeCount = 0;
-    }
-
-
-    public void draw(MouseEvent event) {
-
-        double x = event.getX();
-        double y = event.getY();
-        switch (event.getEventType().getName()) {
-            case "MOUSE_PRESSED":
-                currentShape = currentShape.clone();
-                isDrawing = true;
-                currentShape.setStartX(x);
-                currentShape.setStartY(y);
-                currentShape.setEndX(x);
-                currentShape.setEndY(y);
-                break;
-            case "MOUSE_DRAGGED":
-
-                currentShape.setEndX(x);
-                currentShape.setEndY(y);
-                this.redrawShapes();
-                break;
-            case "MOUSE_RELEASED":
-                isDrawing = false;
-                this.addShape(currentShape);
-                break;
-        }
-    }
-
-    public void setCurrentShape(Shape currentShape) {
-        this.currentShape = currentShape;
-    }
+  public void setCurrentShape(Shape currentShape) {
+    this.currentShape = currentShape;
+  }
 }
